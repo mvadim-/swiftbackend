ae2c9a0280dc74aeca7d7594ff1d5a95d68f075a
Vadym Maslov <mvadim@me.com>
2016-11-15 12:59:03 +0200

    Add SSL support.

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,11 +5,17 @@ HeliumLogger.use()
 
 let router = Router()
 
+let myCertPath = "/tmp/Creds/Self-Signed/cert.pem"
+let myKeyPath = "/tmp/Creds/Self-Signed/key.pem"
+
+let mySSLConfig =  SSLConfig(withCACertificateDirectory: nil, usingCertificateFile: myCertPath, withKeyFile: myKeyPath, usingSelfSignedCerts: true)
+
+
 router.get("/") {
-   request, response, next in
-   response.send("Hello, Orest!")
-   next()
+    request, response, next in
+    response.send("Hello, Orest!")
+    next()
 }
 
-Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.addHTTPServer(onPort: 8090, with: router, withSSL: mySSLConfig)
 Kitura.run()
